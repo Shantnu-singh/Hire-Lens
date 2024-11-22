8878e31f07db49eb77d04a0250b1f29cf7fe65cd
Shantnu-singh <singhshantnu2001@gmail.com>
2024-11-22 22:50:51 +0530

    added readme.md

--- pdf2img.py
+++ pdf2img.py
@@ -1,13 +1,17 @@
 from pdf2image import convert_from_bytes , convert_from_path 
 import io
+import os
 from PIL import Image
 import base64
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def convert_to_img(upload_pdf):
     try:
         if upload_pdf is None:
             raise ValueError("Uploaded file is not there...")
-        img_list = convert_from_bytes(upload_pdf.read() , poppler_path="C:/Users/singh/Release-24.08.0-0/poppler-24.08.0/Library/bin")
+        img_list = convert_from_bytes(upload_pdf.read() , poppler_path=os.getenv("POPPER_DIRECTORY_PATH"))
         front_page =  img_list[0]
         # front_page.save("resume/page1.png")
         
